249c5a3966aa5e86b56171a3a37ba7488a7ef21b
Jamie <jamieyu96@gmail.com>
2018-06-20 15:37:46 -0400

    reorganization of code

--- client.js
+++ client.js
@@ -1,45 +1,49 @@
+// API key required to access the food2fork API.
 const API_KEY = '5b5db650cd471fa8a983a16c32b6ebb9';
 
 /**
- * Find a random recipe given the user's input choices.
+ * onClick() function used to retrieve a random recipe from the food2fork API.
  */
 function findRecipe() {
     let recipeParameters = generateIngredients();
     
-    // Call helper function to construct API call.
+    // Make first AJAX request for a list of recipe IDs.
     $.ajax({
       url: "/recipe-id",
-      data: { q: recipeParameters },
-      type: "get", //send it through get method
+      data: { q: recipeParameters }, // The ingredients selected by the user.
+      type: "get", 
       success: function(response) {
-        const randInt = Math.floor(Math.random() * response.recipes.length) + 1;
-        let recipe = response.recipes[randInt];
-        console.log(recipe);
-        const rId = recipe.recipe_id;
-  
-        // Use selected recipe to make second AJAX request for the recipe.
-        $.ajax({
-            url: "/recipe-details",
-            data: { rId: rId },
-            type: "get",
-            success: function(response) { 
-                console.log(response);
-                displayRecipeResults(response);
-            },
-            error: function(xhr) {
-                console.log(xhr);
-            }
-        });
+        // If no recipe is found from the database, display error message.
+        if (response.recipes.length === 0) {
+            const errorDiv = document.getElementById('error-message');
+            errorDiv.innerHTML += '<h4>No recipes found. Try different ingredients</h4>';
+        } else { 
+            const randInt = Math.floor(Math.random() * response.recipes.length) + 1;
+            let recipe = response.recipes[randInt];
+            const rId = recipe.recipe_id;
+
+            // Use selected recipe ID to make second AJAX request for the recipe details.
+            $.ajax({
+                url: "/recipe-details",
+                data: { rId: rId },
+                type: "get",
+                success: function(response) { 
+                    displayRecipeResults(response);
+                },
+                error: function(xhr) {
+                    console.log(xhr);
+                }
+            });
+        }
       },
       error: function(xhr) {
         console.log(xhr); 
       }
     }); 
-    // Retrieve 1 random recipe and parse result object for ingredient list and recipe instructions
 }
 
 /**
- * Generate a list of ingredients from input forms.
+ * Helper function used to generate a list of ingredients from input forms.
  */
 function generateIngredients() {
     let meat = (Array.from(document.getElementsByName("meatList")).find(r => r.checked).value);
@@ -47,12 +51,10 @@ function generateIngredients() {
     return meat + ',' + vegetable;
 }
 
+/**
+ * Display the results of the ingredient search by hiding the input form and displaying the selected recipe.
+ */
 function displayRecipeResults(response) {
-    let recipeTitle = response.recipe.title;
-    let ingredientsList = response.recipe.ingredients;
-    let imgUrl = response.recipe.image_url;
-    let recipeLink = response.recipe.source_url;
-    
     // Find search container and hide it.
     let searchDiv = document.getElementById('search');
     searchDiv.style.display = "none";
@@ -60,19 +62,41 @@ function displayRecipeResults(response) {
     // Find results container and display it.
     let resultsDiv = document.getElementById('results');
     resultsDiv.style.display = "block";
+ 
+    clearOldRecipe();   
+    addNewRecipe(response.recipe.title, response.recipe.image_url, response.recipe.ingredients, response.recipe.source_url);
+}
 
+/**
+ * Helper function used to remove the ingredient input forms.
+ */
+function clearOldRecipe() {
     // Find div containers to append ingredients, image, and recipe link.
-    let titleDiv = document.getElementById('recipe-title');
-    let ingredientsDiv = document.getElementById('ingredients-list');
-    let imageDiv = document.getElementById('recipe-img');
-    let recipeLinkDiv = document.getElementById('recipe-link');
-    
-    // Empty divs before appending.
+    const titleDiv = document.getElementById('recipe-title');
+    const ingredientsDiv = document.getElementById('ingredients-list');
+    const imageDiv = document.getElementById('recipe-img');
+    const recipeLinkDiv = document.getElementById('recipe-link');
+
     titleDiv.innerHTML = "";
     ingredientsDiv.innerHTML = ""; 
     imageDiv.innerHTML = ""; 
     recipeLinkDiv.innerHTML = "";
-    
+}
+
+/**
+ * Helper function used to add the new recipe to the page.
+ * @param {!String} recipeTitle The title of the recipe.
+ * @param {!String} imgUrl The URL to the image associated with the recipe.
+ * @param {!Array<String> ingredientsList List of ingredients used in the recipe.
+ * @param {!String} recipeLink The URL to the original recipe.
+ */
+function addNewRecipe(recipeTitle, imgUrl, ingredientsList, recipeLink) {
+    // Find div containers to append ingredients, image, and recipe link.
+    const titleDiv = document.getElementById('recipe-title');
+    const ingredientsDiv = document.getElementById('ingredients-list');
+    const imageDiv = document.getElementById('recipe-img');
+    const recipeLinkDiv = document.getElementById('recipe-link');
+
     // Append ingredient list, recipe image, and link to full recipe.
     titleDiv.innerHTML += '<h3>' + recipeTitle + '</h3>';
     ingredientsList.forEach(ingredient => {
@@ -81,3 +105,4 @@ function displayRecipeResults(response) {
     imageDiv.innerHTML += '<img src=' + imgUrl + ">";
     recipeLinkDiv.innerHTML += '<a href="' + recipeLink + '" target="_blank">Full Recipe</a>';
 }
+
